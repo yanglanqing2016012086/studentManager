17a62d1956b684bc25c2f9759d3381d701577575
chenqi2016012003 <2506260356@qq.com>
2019-05-04 19:14:30 +0800

    小改动

--- mystudentManage/StudentManage.java
+++ mystudentManage/StudentManage.java
@@ -97,7 +97,42 @@ public class StudentManage {
 	}
 	//添加类
 	public static void addstudent(ArrayList<Student> arry) {
-		//创建键盘录入对象
+		//录入对象
+		Scanner sc= new Scanner(System.in);
+		int stuId;
+		while(true) {
+			System.out.println("请输入学号：");
+			stuId = sc.nextInt();
+			boolean flag = false;
+			for(int i=0;i<arry.size();i++) {
+				Student s = arry.get(i);
+				if(s.getID()==stuId) {		
+					flag = true;			
+					break;
+				}
+			}
+			if(flag) {
+				System.out.println("您输入的账号被占用！！！");
+			}else {
+				break;
+			}
+		}
+		System.out.println("请输入姓名：");
+		String name1 = sc.nextLine();
+		String name = sc.nextLine();
+		System.out.println("请输入出生日期：");
+		String birDate = sc.nextLine();
+		System.out.println("请输入性别，false代表女，true代表男：");
+		boolean gender = sc.nextBoolean();
+		//创建学生对象
+		Student s = new Student();
+		s.setID(stuId);
+		s.setName(name);
+		s.setBirDate(birDate);
+		s.setGender(gender);
+		//把学生对象添加到集合
+		arry.add(s);
+		System.out.println("添加学生成功！");
 		
 	}
 	//删除类
@@ -121,6 +156,35 @@ public class StudentManage {
 	}
 	//修改类
 	public static void changestudent(ArrayList<Student> arry) {
-		
+		Scanner sc= new Scanner(System.in);
+		System.out.print("请输入要改变的学号：");
+		int changeId = sc.nextInt();
+		int index = -1;
+		for(int i=0;i<arry.size();i++) {
+			Student s = arry.get(i);
+			if(s.getID()==changeId) {		
+				index = i;				
+			}
+		}
+		if (index == -1) {
+			System.out.println("您要改变的学生不存在，请重新操作！");
+		} else {
+			System.out.println("请输入新姓名：");
+			String name1 = sc.nextLine();
+			String name = sc.nextLine();
+			System.out.println("请输入新出生日期：");
+			String birDate = sc.nextLine();
+			System.out.println("请输入新性别，false代表女，true代表男：");
+			boolean gender = sc.nextBoolean();
+			//创建学生对象
+			Student s = new Student();
+			s.setID(changeId);
+			s.setName(name);
+			s.setBirDate(birDate);
+			s.setGender(gender);
+			//修改学生对象
+			arry.set(index,s);
+			System.out.println("修改学生成功！！！");
+		}
 	}
 }
